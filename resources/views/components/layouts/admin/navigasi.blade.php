a32f3a5005df757b96050c7f1e27c94be7b2d05c
JayStphn <jayawisesa14@gmail.com>
2025-07-02 12:47:58 +0800

    update navigasi

--- resources/views/components/layouts/admin/navigasi.blade.php
+++ resources/views/components/layouts/admin/navigasi.blade.php
@@ -1,6 +1,6 @@
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
     <h1 class="h2 text-primary"><b>PASTI</b></h1>
-    <h2 class="text-justify text-muted"><b class="text-primary">Selamat Datang, </b>{{ auth()->user()->nama }}</h2>
+    <h2 class="text-justify text-muted"><b class="text-primary">Halo, </b>{{ auth()->user()->nama }}</h2>
     <div class="btn-toolbar mb-2 mb-md-0">
         <div class="btn-group mr-2">
             <a href="{{ route('profile') }}" class="btn btn-primary text-white">Profile</a>
